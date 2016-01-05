5b547fdd4ecbed04eb1fccb19be36ac204624229
destiny1020 <ruixiang.j@cn.micropoplar.com>
2016-01-05 16:43:35 +0800

    setup restful service for mmr and add banner loading directive

---
+++ mmr-ws/src/test/java/com/micropoplar/mmr/MmrWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.micropoplar.mmr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MmrWsApplication.class)
+public class MmrWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ www/js/directives/imageDirectives.js
@@ -0,0 +1,25 @@
+angular.module('mmr.directives')
+
+.directive('bannerPreload', ['$rootScope', function($rootScope) {
+    return {
+      restrict: 'A',
+      scope: {
+        ngSrc: '@'
+      },
+      link: function(scope, element, attrs) {
+
+        element.on('load', function() {
+          $rootScope.bannerLoading = false;
+          element.addClass('in');
+          $rootScope.$apply();
+        }).on('error', function() {
+          $rootScope.bannerLoading = false;
+        });
+
+        scope.$watch('ngSrc', function(newVal) {
+          $rootScope.bannerLoading = true;
+          element.removeClass('in');
+        });
+      }
+    };
+}]);

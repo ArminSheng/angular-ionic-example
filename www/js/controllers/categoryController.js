02de6e7e0b90943877915d2a49e65e1c781d27d6
destiny1020 <ruixiang.j@cn.micropoplar.com>
2016-01-09 16:25:50 +0800

    filter component and css animation

--- www/js/controllers/categoryController.js
+++ www/js/controllers/categoryController.js
@@ -1,6 +1,26 @@
 angular.module('mmr.controllers')
 
-.controller('CategoryCtrl', ['$scope', '$rootScope', '$ionicHistory', 
+.controller('CategoryCtrl', ['$scope', '$rootScope', '$ionicHistory',
   function($scope, $rootScope, $ionicHistory) {
-  
+
+  // controller defaults
+  $scope.filtersActivated = false;
+  $scope.selectedFilterIndex = 0;
+  $scope.filters = [
+    { 'text': '智能排序' },
+    { 'text': '价格从高到低' },
+    { 'text': '价格从低到高' },
+    { 'text': '销量从高到低' },
+    { 'text': '销量从低到高' }
+  ]
+
+  $scope.activateFilters = function() {
+    $scope.filtersActivated = !$scope.filtersActivated;
+  };
+
+  $scope.doSelectFilter = function(idx) {
+    $scope.selectedFilterIndex = idx;
+    $scope.filtersActivated = false;
+  };
+
 }]);

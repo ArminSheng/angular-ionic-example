5d7aa0317d7d9a756aa9efce4d3ee5459916c363
destiny1020 <ruixiang.j@cn.micropoplar.com>
2016-01-10 18:20:19 +0800

    add anim for the toggle on screen popup

--- www/js/controllers/categoryController.js
+++ www/js/controllers/categoryController.js
@@ -16,6 +16,10 @@ angular.module('mmr.controllers')
 
   $scope.screenActivated = false;
 
+  // record which brand and attribute has been selected
+  var selectedBrandsIdx = {},
+      selectedAttributesIdx = {};
+
   // methods
   $scope.activateSort = function() {
     $scope.screenActivated = false;
@@ -35,9 +39,34 @@ angular.module('mmr.controllers')
     $rootScope.$root.ui.tabsHidden = !$rootScope.$root.ui.tabsHidden;
   };
 
+  $scope.doSelectBrand = function(idx) {
+    toggleStatus(selectedBrandsIdx, idx);
+  };
+
+  $scope.doSelectAttribute = function(idx) {
+    toggleStatus(selectedAttributesIdx, idx);
+  };
+
+  $scope.checkStatus = function(type, idx) {
+    if(type === 'brand') {
+      return !!selectedBrandsIdx[idx];
+    } else {
+      return !!selectedAttributesIdx[idx];
+    }
+  };
+
   // cache bindings
   localStorageService.bind($scope, 'brands');
   localStorageService.bind($scope, 'attributes');
 
   // event handlers
+
+  // private functions
+  function toggleStatus(mapping, idx) {
+    if(mapping[idx]) {
+      delete mapping[idx];
+    } else {
+      mapping[idx] = true;
+    }
+  }
 }]);
